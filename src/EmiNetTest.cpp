0a1cc70dc349e9f4d9220853bddc03ab29f88543
Sven Fink <sven.fink@piclife.de>
2018-02-21 08:45:51 +0100

    Initial commit

---
+++ src/EmiNetTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : EmiNetTest.cpp
+// Author      : Sven Fink
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	return 0;
+}
