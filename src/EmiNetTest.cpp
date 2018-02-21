a2a6821e23ec6663d59d4d385cc87ace427ae34f
Sven Fink <sven.fink@piclife.de>
2018-02-21 11:58:53 +0100

    Allow port as console parameter

--- src/EmiNetTest.cpp
+++ src/EmiNetTest.cpp
@@ -6,10 +6,35 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <args.hxx>
 #include <iostream>
+
 using namespace std;
 
-int main() {
-	cout << "Hello World" << endl; // prints Hello World
-	return 0;
+int main(int argc, char** argv) {
+  args::ArgumentParser parser("Extensible Media-Independent Network Gateway",
+                              "For those about to rock!");
+  args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+  args::ValueFlag<int> port(parser, "port", "Port of the WebInterface",
+                            {'p', "port"}, 80);
+
+  try {
+    parser.ParseCLI(argc, argv);
+  } catch (args::Help&) {
+    std::cout << parser;
+    return 0;
+  } catch (args::ParseError& e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
+  } catch (args::ValidationError& e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
+  }
+
+  cout << "Running on Port: " << args::get(port) << endl;
+
+  cout << "Hello World" << endl;  // prints Hello World
+  return 0;
 }
